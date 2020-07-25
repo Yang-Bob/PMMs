31ba0c6c32c7401be4d564c8a1e199cdcbefc5cd
Yang-Bob <48110732+Yang-Bob@users.noreply.github.com>
2020-07-25 09:56:01 +0800

    Create train_group3.sh

---
+++ scripts/train_group3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ..
+
+GPU_ID=3
+GPOUP_ID=3
+
+CUDA_VISIBLE_DEVICES=$GPU_ID python train.py \
+    --group=${GPOUP_ID} \
+    --num_folds=4 \
+    --arch=FPMMs \
+    --lr=3.5e-4 \
+    --dataset='voc'
+

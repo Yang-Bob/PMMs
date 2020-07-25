a5f155b19a3f3c18cd004379b4f605720d5013e9
Yang-Bob <48110732+Yang-Bob@users.noreply.github.com>
2020-07-25 09:51:58 +0800

    Update train_group0.sh

--- scripts/train_group0.sh
+++ scripts/train_group0.sh
@@ -2,12 +2,12 @@
 
 cd ..
 
-GPU_ID=4
+GPU_ID=0
 GPOUP_ID=0
 
 CUDA_VISIBLE_DEVICES=$GPU_ID python train.py \
     --group=${GPOUP_ID} \
     --num_folds=4 \
-    --arch=FewSg_v0_0 \
+    --arch=FPMMs \
     --lr=3.5e-4 \
     --dataset='voc'
